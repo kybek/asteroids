c8e9082683cb736aed0642739028a3ff6e01a5f6
EnoughSensei <EnoughSensei@github.com>
2019-12-09 15:50:48 +0300

    Commit

--- World/World.gd
+++ World/World.gd
@@ -86,7 +86,7 @@ var elapsed_time := 0.0
 
 func _process (delta):
 	elapsed_time += delta
-	print(elapsed_time)
+#	print(elapsed_time)
 	
 	for child in get_children():
 		if child is KinematicBody2D:
@@ -95,4 +95,4 @@ func _process (delta):
 
 func _on_Ship_death():
 	print(elapsed_time)
-	queue_free()
+#	queue_free()
